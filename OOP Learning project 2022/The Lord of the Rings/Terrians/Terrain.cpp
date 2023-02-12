f67a79cf5e81376fd2b22c95ea84cd2f28920e2f
nathan215 <45839865+nathan215@users.noreply.github.com>
2023-02-12 20:46:55 +0800

    Add files via upload

---
+++ OOP Learning project 2022/The Lord of the Rings/Terrians/Terrain.cpp
@@ -0,0 +1,12 @@
+#include "Terrain.h"
+
+//write your codes here
+
+Terrain:: Terrain(int x, int y) : MapUnit(x, y){}
+
+Terrain:: ~Terrain(){}
+
+UnitType Terrain:: get_unit_type() const
+{
+    return UnitType::TERRAIN;
+}
